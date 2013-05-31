429ff6f2e7fb2b32aabe7fb7446b36a20e3510b7
vkislichenko <vkislichenko@github.com>
2013-06-01 02:23:09 +0300

    returning expected results, fixing errors in PHPUnit tests

--- tests/presenceTest.php
+++ tests/presenceTest.php
@@ -5,7 +5,8 @@ $pubnub = new \Pubnub\Pubnub( 'demo', 'demo', false , false, false );
 $pubnub->presence(array(
     'channel'  => 'testChannel',
     'callback' => function($message) {
-        $fp = fopen('presenceOut.txt', 'w');
+		$filePath = sys_get_temp_dir() . '/presenceOut.txt';
+        $fp = fopen($filePath, 'w');
         fwrite($fp, serialize($message));
         fclose($fp);
         exit;
--- tests/subscribeTest.php
+++ tests/subscribeTest.php
@@ -5,7 +5,9 @@ $pubnub = new \Pubnub\Pubnub( 'demo', 'demo', false , false, false );
 $pubnub->subscribe(array(
     'channel'  => 'testChannel',
     'callback' => function($message) {
-        $fp = fopen('subscribeOut.txt', 'w');
+		$filePath = sys_get_temp_dir() . '/subscribeOut.txt';
+		
+        $fp = fopen($filePath, 'w');
         fwrite($fp, serialize($message));
         fclose($fp);
         exit;
